2991266d569fb1d7074d5410cbc43d8585b4ec2f
Vấn Lê <vanlh@itpsolution.net>
2019-11-26 21:27:54 +0700

    init project

---
+++ YouTubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by mac on 11/26/19.
+//  Copyright © 2019 lhvan89. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
